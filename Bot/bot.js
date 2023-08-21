2704fc93806d7c66a7afffd295b77f64e6709896
Filipe Recharte <filipeteixeira00@hotmail.com>
2023-08-21 03:58:47 +0100

    add support to choose rates and api error handling

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,17 +1,18 @@
 const axios = require('axios');
 
-const currencyPairs = (process.env.CURRENCY_PAIRS || "BTC-USD").split(",");
-const fetchInterval = parseInt(process.env.FETCH_INTERVAL) || 5;
-const oscillation = parseFloat(process.env.OSCILLATION_PERCENTAGE) || 0.01;
-
+var botConfig = {
+    currencyPairs: (process.env.CURRENCY_PAIRS || "BTC-USD").split(","),
+    fetchInterval: parseInt(process.env.FETCH_INTERVAL) || 5,
+    oscillation: parseFloat(process.env.OSCILLATION_PERCENTAGE) || 0.01,
+    rate: parseFloat(process.env.RATE) || "BID",
+}
 var initialRate = new Map();
-var fetching = setInterval(fetch, fetchInterval * 1000);
+var fetching = setInterval(fetch, botConfig.fetchInterval * 1000);
 
 fetch();
 
 function fetch(){
-    for (const currencyPair of currencyPairs) {
-        console.log(`Fetching ${currencyPair}...`);
+    for (const currencyPair of botConfig.currencyPairs) {
         axios.get(`https://api.uphold.com/v0/ticker/${currencyPair}`)
         .then(
             function (response) {
@@ -29,32 +30,50 @@ function fetch(){
         )
         .catch(
             function (error) {
-                console.log(error.message);
+                console.log(error)
+                handleErrors(error.response.data, currencyPair);
             }
         )
     }
 }
 
 function setupFetch(currentFetch) {
-    initialRate.set(currentFetch.currencyPair,{
+    initialRate.set(currentFetch.currencyPair, {
         bid: currentFetch.bidRate,
-        ask: currentFetch.askRate
+        ask: currentFetch.askRate,
     });
 }
 
 function handleFetch(currentFetch){
-    currentInitialRate = initialRate.get(currentFetch.currencyPair);
+    currentInitialRate = botConfig.rate == 'BID' ? initialRate.get(currentFetch.currencyPair).bid : initialRate.get(currentFetch.currencyPair).ask;
+    currentFetchRate = botConfig.rate == 'BID' ? currentFetch.bidRate : currentFetch.askRate;
 
     // Calculate the percentage change
-    currentFetch.percentageChange = ((currentFetch.bidRate - currentInitialRate.bid) / currentInitialRate.bid) * 100;
+    currentFetch.percentageChange = ((currentFetchRate - currentInitialRate) / currentInitialRate) * 100;
 
-    // Log the percentage change
-    if (Math.abs(currentFetch.percentageChange).toFixed(2) >= oscillation) {
-        if (currentFetch.percentageChange > 0) {
-            console.log(`[ ${currentFetch.date} ] ${currentFetch.currencyPair} BID PRICE INCREASED ${currentFetch.percentageChange.toFixed(2)}%`);
-        } else {
-            console.log(`[ ${currentFetch.date} ] ${currentFetch.currencyPair} BID PRICE DECREASED ${Math.abs(currentFetch.percentageChange).toFixed(2)}%`);
-        }
+    if (Math.abs(currentFetch.percentageChange).toFixed(2) >= botConfig.oscillation) {
+        logAlert(currentFetch);
+    }
+}
+
+function logAlert(currentFetch) {
+    if (currentFetch.percentageChange > 0) {
+        console.log(`[ ${currentFetch.date} ] ${currentFetch.currencyPair} ${botConfig.rate} PRICE INCREASED ${currentFetch.percentageChange.toFixed(2)}%`);
+    } else {
+        console.log(`[ ${currentFetch.date} ] ${currentFetch.currencyPair} ${botConfig.rate} PRICE DECREASED ${Math.abs(currentFetch.percentageChange).toFixed(2)}%`);
+    }
+}
+
+function handleErrors(error, currencyPair) {
+    switch (error.code){
+        case "not_found":
+            console.log(`Currency pair ${currencyPair} not found. Removing from list.`);
+            botConfig.currencyPairs.splice(botConfig.currencyPairs.indexOf(currencyPair), 1);
+            break;
+        case "too_many_requests":
+            console.log("Too many requests. Increasing fetch interval by 5s.");
+            botConfig.fetchInterval += 5;
+            break;
     }
 }
 
